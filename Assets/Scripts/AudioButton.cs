c21d8b2695b8ffc8971e09a0e65388ba34b9f1a5
Joao Vitor Oliveira Barbosa <6623140@univali.br>
2018-04-03 15:39:29 -0300

    First commit

---
+++ Assets/Scripts/AudioButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioButton : MonoBehaviour {
+
+	private GameController gameController;
+
+	// Use this for initialization
+	void Start () 
+	{
+		gameController = FindObjectOfType<GameController> ();
+	}
+
+
+	public void HandleClick()
+	{
+		gameController.AudioButtonClicked ();
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public Text questionDsiplayText;
+	public Text scoreDisplayText;
+	public Text timeRemainingDisplayText;
+	public SimpleObjectPool answerButtonObjectPool;
+	public Transform answerButtonParent;
+	public GameObject questionDisplay;
+	public GameObject roundEndDisplay;
+	public GameObject audioButton;
+
+	private AudioSource audioSource;
+	private DataController dataController;
+	private RoundData currentRoundData;
+	private QuestionData[] questionPool;
+
+	private bool isRoundActive;
+	private float timeRemaining;
+	private int questionIndex;
+	private int playerScore;
+	private List<GameObject> answerButtonGameObjetcs = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		dataController = FindObjectOfType<DataController> ();
+		currentRoundData = dataController.GetCurrentRoundData ();
+		questionPool = currentRoundData.questions;
+		timeRemaining = currentRoundData.timeLimitInSecond;
+		UpdateTimeRemainingDisplay ();
+
+		playerScore = 0;
+		questionIndex = 0;
+
+		ShowQuestion ();
+		isRoundActive = true;
+
+	}
+		
+	private void ShowQuestion()
+	{
+		RemoveAnswerButtons ();
+		QuestionData questionData = questionPool [questionIndex];
+
+		// TODO
+		audioSource = audioButton.GetComponent<AudioSource>();
+		audioSource.clip = questionData.questionAudio;
+		audioSource.Play ();
+
+		for (int i = 0; i < questionData.answers.Length; i++)
+		{
+			GameObject answerButtonGameObject = answerButtonObjectPool.GetObject();
+			answerButtonGameObjetcs.Add(answerButtonGameObject);
+			answerButtonGameObject.transform.SetParent(answerButtonParent);
+
+			AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton>();
+			answerButton.Setup(questionData.answers[i]);
+		}
+	}
+
+	private void RemoveAnswerButtons()
+	{
+		while (answerButtonGameObjetcs.Count > 0) 
+		{
+			answerButtonObjectPool.ReturnObject (answerButtonGameObjetcs [0]);
+			answerButtonGameObjetcs.RemoveAt (0);
+		}
+	}
+	public void AnswerButtonClicked(bool isCorrect)
+	{
+		if (isCorrect) 
+		{
+			playerScore += currentRoundData.pointsAddedForCorrectAnswer;
+			scoreDisplayText.text = "Score: " + playerScore.ToString ();
+			timeRemaining += currentRoundData.addedTimeForCorrectAnswer;
+
+		}
+
+		if (questionPool.Length > questionIndex + 1) {
+			questionIndex++;
+			ShowQuestion ();
+		} else 
+		{
+			EndRound ();
+		}
+
+	}
+
+	public void AudioButtonClicked ()
+	{ 
+		audioSource.Play ();
+	}
+
+	public void EndRound()
+	{
+		isRoundActive = false;
+
+		questionDisplay.SetActive (false);
+		roundEndDisplay.SetActive (true);
+	}
+
+	public void ReturnToMenu ()
+	{
+		SceneManager.LoadScene ("MenuScreen");
+	}
+
+
+	private void UpdateTimeRemainingDisplay()
+	{
+		timeRemainingDisplayText.text = "Time: " + Mathf.Round (timeRemaining).ToString ();
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (isRoundActive) 
+		{
+			timeRemaining -= Time.deltaTime;
+			UpdateTimeRemainingDisplay ();
+
+			if (timeRemaining <= 0f) 
+			{
+				EndRound ();
+			}
+
+		}
+
+	}
+}
