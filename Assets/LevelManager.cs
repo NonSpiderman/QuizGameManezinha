b56c66d48d4645608cb109371f0b4acc64420f4f
Vanessa Felix Christoff <6605036@univali.br>
2018-06-05 18:13:52 -0300

    Update: Scenes added, Level Manager made, reorganizing stuff

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void StartOmanhe() {
+		SceneManager.LoadScene ("Omanhe");
+	}
+
+	public void StartTurishmo () {
+		SceneManager.LoadScene ("Turishmo");
+	}
+
+	public void StartInglesh () {
+		SceneManager.LoadScene ("Inglesh");
+
+	}
+
+	public void StartManezes () {
+		SceneManager.LoadScene ("Manezes");
+
+	}
+}
--- Assets/Scripts/SimpleObjectPool.cs
+++ Assets/Scripts/SimpleObjectPool.cs
@@ -2,56 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// A very simple object pooling class
-//is attached to the AnswerButton prefab
 public class SimpleObjectPool : MonoBehaviour {
-	// the prefab that this object pool returns instances of
+	
 	public GameObject prefab;
-	// collection of currently inactive instances of the prefab
 	private Stack<GameObject> inactiveInstances = new Stack<GameObject>();
 
-	// Returns an instance of the prefab
 	public GameObject GetObject() {
 		GameObject spawnedGameObject;
 
-		//count returns the total count of elements of the stack
-		//if there is an inactive instance of the prefab ready to return, return that
 		if (inactiveInstances.Count > 0) {
-			//remove the instance from the collection of inactive instances
 			spawnedGameObject = inactiveInstances.Pop();
-			//removes and returns items from the top of the stack
 		}
-		// otherwise, create a new instance
 		else {
 			spawnedGameObject = (GameObject)GameObject.Instantiate(prefab, this.transform.parent);
 		
-			//add the PooledObject component to the prefab so we know it came from this pool
 			PooledObject pooledObject = spawnedGameObject.AddComponent<PooledObject>();
 			pooledObject.pool = this;
-			//creates a new pooledObject instance equals to its reference in the script
-			//equals pool to the instance of this class, which is indeed this class
 		}
 
-		//enable the instance
 		spawnedGameObject.SetActive(true);
 
-		//return a reference to the instance
 		return spawnedGameObject;
 	}
 
-	//Return an instance of the prefab to the pool
 	public void ReturnObject(GameObject toReturn) {
 		PooledObject pooledObject = toReturn.GetComponent<PooledObject>();
 
-		//if the instance came from this pool, return it to the pool
 		if(pooledObject != null && pooledObject.pool == this) {
-			//disable the instance
 			toReturn.SetActive(false);
 
-			//add the instance to the collection of inactive instances
 			inactiveInstances.Push(toReturn);
 		}
-		//otherwise, just destroy it
 		else
 		{
 			Debug.LogWarning(toReturn.name + " was returned to a pool it wasn't spawned from! Destroying.");
@@ -60,7 +41,6 @@ public class SimpleObjectPool : MonoBehaviour {
 	}
 }
 
-//a component that simply identifies the pool that a GameObject came from
 public class PooledObject : MonoBehaviour {
 	public SimpleObjectPool pool;
 }
